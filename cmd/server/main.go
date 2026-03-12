b86a513cfab6551aeb8285ad50a620659e6e8b46
yuanzhihao <yuanzhihao1996@outlook.com>
2026-03-12 08:48:51 +0800

    feat: register host/credential/danger-rule routes in main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,7 +9,10 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/sshive/sshive/internal/auth"
 	"github.com/sshive/sshive/internal/config"
+	"github.com/sshive/sshive/internal/credential"
+	"github.com/sshive/sshive/internal/dangerrule"
 	"github.com/sshive/sshive/internal/db"
+	"github.com/sshive/sshive/internal/host"
 	"github.com/sshive/sshive/internal/tenant"
 	"github.com/sshive/sshive/internal/user"
 )
@@ -56,6 +59,25 @@ func main() {
 	authed.GET("/users", auth.RequirePermission("user:manage"), uh.List)
 	authed.POST("/users", auth.RequirePermission("user:manage"), uh.Create)
 	authed.PUT("/users/:id/roles", auth.RequirePermission("user:manage"), uh.AssignRoles)
+
+	// 凭据管理
+	credH := credential.NewHandler()
+	authed.GET("/credentials", auth.RequirePermission("host:manage"), credH.List)
+	authed.POST("/credentials", auth.RequirePermission("host:manage"), credH.Create)
+
+	// 主机管理
+	hostH := host.NewHandler()
+	authed.GET("/hosts", auth.RequirePermission("host:manage"), hostH.List)
+	authed.POST("/hosts", auth.RequirePermission("host:manage"), hostH.Create)
+	authed.PUT("/hosts/:id", auth.RequirePermission("host:manage"), hostH.Update)
+	authed.DELETE("/hosts/:id", auth.RequirePermission("host:manage"), hostH.Delete)
+
+	// 高危规则管理
+	ruleH := dangerrule.NewHandler()
+	authed.GET("/danger-rules", auth.RequirePermission("rule:manage"), ruleH.List)
+	authed.POST("/danger-rules", auth.RequirePermission("rule:manage"), ruleH.Create)
+	authed.PUT("/danger-rules/:id", auth.RequirePermission("rule:manage"), ruleH.Update)
+	authed.DELETE("/danger-rules/:id", auth.RequirePermission("rule:manage"), ruleH.Delete)
 
 	addr := fmt.Sprintf(":%d", config.C.Port)
 	slog.Info("server starting", "addr", addr)
